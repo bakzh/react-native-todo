333f6ac1a4e731a8e167971217bd7dd024209f7f
bakzh <park3096118@gmail.com>
2022-08-25 12:48:09 +0900

    초기파일

---
+++ src/images.js
@@ -0,0 +1,11 @@
+import CkeckBox from '../assets/icons/check_box.png';
+import UnCkeckBox from '../assets/icons/unckeck_box.png';
+import Delete from '../assets/icons/delete.png';
+import Update from '../assets/icons/update.png';
+
+export const images = {
+    completed: CkeckBox,
+    uncompleted: UnCkeckBox,
+    delete: Delete,
+    update: Update
+}
